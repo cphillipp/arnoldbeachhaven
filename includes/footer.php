a3a190564c333d011c4cd69ade52cbd4eeb1466c
unknown <phillipp@.(none)>
2011-10-04 14:06:51 -0600

    added site files

---
+++ includes/footer.php
@@ -0,0 +1,14 @@
+<div class="footer">
+	<div class="left-side">
+		<a href="index.php">Home</a><span>|</span>
+		<a href="sandy-shores.php">Sandy Shores</a><span>|</span>
+		<a href="barefoot-sands.php">Barefoot Sands</a><span>|</span>
+		<a href="rates.php">Rates</a><span>|</span>
+		<a href="about-us.php">About Us</a><span>|</span>
+		<a href="calendar.php">Availability Calendar</a><span>|</span>
+		<a href="photos.php">Photos</a>
+	</div>
+	<div class="right-side">
+		<p>&copy; Arnold Beach Haven <span>&bull;</span> <a href="mailto:sharon@arnoldbeachhaven.com">sharon@arnoldbeachhaven.com</a></p>
+	</div>
+</div>
