a8bc8c1b720c17350c6d6baa929df01e9f656c90
Clayton Phillipp <phillipp@Claytons-MacBook-Pro.local>
2012-07-12 14:53:21 -0600

    updated site files

---
+++ sandy-shores.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="robots" content="INDEX,FOLLOW" />
+	
+	<script type="text/javascript" src="js/prototype.js"></script>
+	<script type="text/javascript" src="js/scriptaculous.js?load=effects,builder"></script>
+	<script type="text/javascript" src="js/lightbox.js"></script>
+
+	<link rel="stylesheet" type="text/css" href="css/layout.css" media="all" />
+	<link rel="stylesheet" href="css/lightbox.css" type="text/css" media="screen" />
+
+	<title>Arnold Beach Haven | Oregon Coast Beach Front Rental's</title>
+</head>
+
+<body>
+	<div class="full-area">
+		<div class="header">
+			<a href="index.php" class="logo"></a>
+			<ul class="nav">
+				<li><a href="index.php">Home</a></li>
+				<li><a href="sandy-shores.php" class="active">Sandy Shores</a></li>
+				<li><a href="barefoot-sands.php">Barefoot Sands</a></li>
+				<li><a href="availability-rates.php">Availability &amp; Rates</a></li>
+				<li><a href="contact-us.php">Contact Us</a></li>
+			</ul>
+		</div>
+		<div class="content">
+			<div class="sub-content narrow">
+				<h1>Sandy Shores</h1>
+				<div class="right-box">
+					<a href="images/house1-front.jpg" rel="lightbox[house1]" title="Sandy Shores Frontal View" class="house1"><img src="images/house1-front.jpg" alt="Sandy Shores Frontal View" /></a>
+					<a href="images/house1-masterbed.jpg" rel="lightbox[house1]" title="Sandy Shores Master Bedroom" class="hide"><img src="images/house1-masterbed.jpg" alt="Sandy Shores Master Bedroom" /></a>
+					<a href="images/house1-masterbed2.jpg" rel="lightbox[house1]" title="Sandy Shores Master Bedroom" class="hide"><img src="images/house1-masterbed2.jpg" alt="Sandy Shores Master Bedroom" /></a>
+					<a href="images/house1-kitchen.jpg" rel="lightbox[house1]" title="Sandy Shores Kitchen" class="hide"><img src="images/house1-kitchen.jpg" alt="Sandy Shores kitchen" /></a>
+					<a href="images/house1-downstairsbed.jpg" rel="lightbox[house1]" title="Sandy Shores Downstairs Bedroom" class="hide"><img src="images/house1-downstairsbed.jpg" alt="Sandy Shores Downstairs Bedroom" /></a>
+					<a href="images/house1-livingroom.jpg" rel="lightbox[house1]" title="Sandy Shores Living Room" class="hide"><img src="images/house1-livingroom.jpg" alt="Sandy Shores Living Room" /></a>
+					<a href="images/house1-back.jpg" rel="lightbox[house1]" title="Sandy Shores Back View" class="hide"><img src="images/house1-back.jpg" alt="Sandy Shores Back View" /></a>
+					<a href="images/house1-back2.jpg" rel="lightbox[house1]" title="Sandy Shores Back View" class="hide"><img src="images/house1-back2.jpg" alt="Sandy Shores Back View" /></a>
+					<a href="images/house1-deck.jpg" rel="lightbox[house1]" title="Sandy Shores Deck View" class="hide"><img src="images/house1-deck.jpg" alt="Sandy Shores Deck View" /></a>
+					<a href="images/house1-beach.jpg" rel="lightbox[house1]" title="Sandy Shores Beach View" class="hide"><img src="images/house1-beach.jpg" alt="Sandy Shores Beach View" /></a>
+					<a href="images/house1-sunset.jpg" rel="lightbox[house1]" title="Sandy Shores Sunset View" class="hide"><img src="images/house1-sunset.jpg" alt="Sandy Shores Sunset View" /></a>
+					<p>Click the image above to begin the slideshow</p>
+				</div>
+				<h2>Things to do at Sandy Shores</h2>
+					<ul>
+						<li><span></span>Sea Lion Caves: 20 miles south</li>
+						<li><span></span>Cape Perpetua: 6 miles south</li>
+						<li><span></span>Local Lighthouses</li>
+						<li><span></span>Oregon Coast Aquarium: 16 miles north</li>
+						<li><span></span>Hatfield Science Center:  16 miles north</li>
+						<li><span></span>Crabbing and Clamming in Alsea Bay:  4 miles north</li>
+						<li><span></span>Deep Sea Fishing on Charter Boats:  16 miles north</li>
+						<li><span></span>Oregon Sand Dunes:  33  miles south</li>
+						<li><span></span>Strawberry Hill, great tide pools &amp; sea lions:  9 miles south</li>
+						<li><span></span>Golfing:  5 miles north</li>
+						<li><span></span>Kite flying</li>
+						<li><span></span>Beach:  right out the back door</li>
+						<li><span></span>Many quaint shops and  fine restaurants are located in Waldport, Yachats, and Newport.</li>
+					</ul>
+				<div class="inner-wrapper">
+					<h2>Information About Sandy Shores</h2>
+					<ul>
+						<li><span></span>The house is a  LARGE OCEANFRONT HOME with a SPECTACULAR VIEW out of many large windows</li>
+						<li><span></span>It has easy beach access, right out the back door!</li>
+						<li><span></span>Sleeps 16 we do allow up to 25 people</li>
+						<li><span></span>High Speed Wireless Internet</li>
+						<li><span></span>Large kitchen with granite and tile counter tops, many work areas perfect for large groups.   It is equipped with all the major appliances; basic cooking, baking and eating utensils. Dishwasher, and Microwave.</li>
+						<li><span></span>5 bedrooms, with a total of 6 queen size beds, and two sets of bunk beds (4 twins)</li>
+						<li><span></span>Bedroom Configuration:  Downstairs; Bedroom#1- has 1 queen bed, loveseat, recliner, TV ,DVD, and VCR.  Upstairs; Bedroom #2, two queen size beds with private deck, bedroom shares bathroom with Bedroom #3 which has one queen size bed, Bedroom #4 has two sets of bunk beds, Bedroom #5  and two queen beds.  Bedrooms 1,2,and 5 have a great oceanfront view.</li>
+						<li><span></span>3 1/2 baths, there are 2 jetted Jacuzzi tubs</li>
+						<li><span></span>2 Cable TV's, 2 VCR's, and 2 DVD players</li>
+						<li><span></span>Fireplace</li>
+						<li><span></span>Large lower deck, and smaller upper deck off of bedroom, both facing the ocean</li>
+						<li><span></span>Washer and dryer, iron and ironing board</li>
+						<li><span></span>Highchair &amp; Pack and Play</li>
+						<li><span></span>Phone</li>
+					</ul>
+					<p>There are NO pets or smoking allowed</p>
+					<p>The house has blankets and pillows.  You will need to bring your own sheets, towels, firewood (if you want to build a fire), paper products- including toilet paper and paper towels, and cleaning rags.  All cleaning supplies are supplied by us, except cleaning rags.  You will be expected to clean the house like you found it as it will be inspected after you leave.  If you prefer to have a cleaning service clean the house it will be an additional charge of $120.00.</p>
+					<a title="Availability Calendar" href="availability-rates.php">Availability &amp; Rates</a>
+				</div>
+				<h1>More Photos</h1>
+				<a href="images/house1-front.jpg" rel="lightbox" title="Sandy Shores Frontal View" class="small-box"><img src="images/house1-front.jpg" alt="Sandy Shores Frontal View" /></a>
+				<a href="images/house1-masterbed.jpg" rel="lightbox" title="Sandy Shores Master Bedroom" class="small-box"><img src="images/house1-masterbed.jpg" alt="Sandy Shores Master Bedroom" /></a>
+				<a href="images/house1-masterbed2.jpg" rel="lightbox" title="Sandy Shores Master Bedroom" class="small-box"><img src="images/house1-masterbed2.jpg" alt="Sandy Shores Master Bedroom" /></a>
+				<a href="images/house1-kitchen.jpg" rel="lightbox" title="Sandy Shores Kitchen" class="small-box"><img src="images/house1-kitchen.jpg" alt="Sandy Shores kitchen" /></a>
+				<a href="images/house1-downstairsbed.jpg" rel="lightbox" title="Sandy Shores Downstairs Bedroom" class="small-box"><img src="images/house1-downstairsbed.jpg" alt="Sandy Shores Downstairs Bedroom" /></a>
+				<a href="images/house1-livingroom.jpg" rel="lightbox" title="Sandy Shores Living Room" class="small-box"><img src="images/house1-livingroom.jpg" alt="Sandy Shores Living Room" /></a>
+				<a href="images/house1-back.jpg" rel="lightbox" title="Sandy Shores Back View" class="small-box"><img src="images/house1-back.jpg" alt="Sandy Shores Back View" /></a>
+				<a href="images/house1-back2.jpg" rel="lightbox" title="Sandy Shores Back View" class="small-box"><img src="images/house1-back2.jpg" alt="Sandy Shores Back View" /></a>
+				<a href="images/house1-deck.jpg" rel="lightbox" title="Sandy Shores Deck View" class="small-box"><img src="images/house1-deck.jpg" alt="Sandy Shores Deck View" /></a>
+				<a href="images/house1-beach.jpg" rel="lightbox" title="Sandy Shores Beach View" class="small-box"><img src="images/house1-beach.jpg" alt="Sandy Shores Beach View" /></a>
+				<a href="images/house1-sunset.jpg" rel="lightbox" title="Sandy Shores Sunset View" class="small-box"><img src="images/house1-sunset.jpg" alt="Sandy Shores Sunset View" /></a>
+			</div>
+		</div>
+		<?php include("includes/footer.php"); ?>
+	</div>
+	
+</body>
+</html>
