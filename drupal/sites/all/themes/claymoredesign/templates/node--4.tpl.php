a86fb0b354021eb858ac01720b56a4c8d1984d24
Clayton Phillipp <phillipp@Claytons-MacBook-Pro.local>
2012-07-17 01:08:56 -0600

    updated contact form

--- drupal/sites/all/themes/claymoredesign/templates/node--4.tpl.php
+++ drupal/sites/all/themes/claymoredesign/templates/node--4.tpl.php
@@ -1,12 +1,12 @@
 <div id="house" class="right-box">
 	<a href="/sites/all/themes/claymoredesign/images/surland.png" title="Surland View" class="surland"><img src="sites/all/themes/claymoredesign/images/surland.png" alt="Surland View" /><span>Surland View - click to enlarge</span></a>
 </div>
-<div class="top push">
+<!-- <div class="top push">
 	<h1>Arnold Beach Haven</h1>
 	<p>Ready to book your vacation, or have questions?</p>
 	<h2><span>Phone:</span> Sharon Arnold - 541-867-7116</h2>
 	<h2><span>Email:</span> <a href="mailto:sharon@arnoldbeachhaven.com">sharon@arnoldbeachhaven.com</a></h2>
-</div>
+</div> -->
 
 <div class="USER four">
 	<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
