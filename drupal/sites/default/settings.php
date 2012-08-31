387f7d5a1fcea8767085681a039b62b5b5d63e66
Clayton Phillipp <phillipp@Claytons-MacBook-Pro.local>
2012-08-31 13:05:46 -0600

    updated images

--- drupal/sites/default/settings.php
+++ drupal/sites/default/settings.php
@@ -361,7 +361,7 @@ ini_set('session.cookie_lifetime', 2000000);
  * between your various domains. Make sure to always start the $cookie_domain
  * with a leading dot, as per RFC 2109.
  */
- $cookie_domain = 'arnoldbeachhaven.pagodabox.com';
+# $cookie_domain = 'arnoldbeachhaven.pagodabox.com';
 
 /**
  * Variable overrides:
